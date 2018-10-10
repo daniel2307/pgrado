bec67e13c539dc8861e6192ae22954774931edd6
daniel <daniel.cutipa@hotmail.com>
2018-10-10 14:00:52 -0400

    cambiando de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import * as firebase from 'firebase';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+const config = {
+  apiKey: "AIzaSyBNz0tduikFJK1AZ-GgRlvEUezpnF2yjto",
+  authDomain: "pgrado-c9d17.firebaseapp.com",
+  databaseURL: "https://pgrado-c9d17.firebaseio.com",
+  projectId: "pgrado-c9d17",
+  storageBucket: "pgrado-c9d17.appspot.com",
+  messagingSenderId: "666766718740"
+};
+firebase.initializeApp(config);
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    HomePage
   ],
   imports: [
     BrowserModule,
@@ -22,8 +30,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    HomePage
   ],
   providers: [
     StatusBar,
